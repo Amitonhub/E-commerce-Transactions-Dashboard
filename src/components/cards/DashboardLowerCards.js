d15603a9277fca0e4c366fce298ef847db20f681
Amit <amitthakurat4615321@gmail.com>
2024-05-23 09:15:34 +0530

    Changes for sales Chart

--- src/components/cards/DashboardLowerCards.js
+++ src/components/cards/DashboardLowerCards.js
@@ -1,40 +1,51 @@
-import React from 'react';
-import { Card } from 'antd';
-import Barchart from '../charts/barchart';
-import Saleschart from '../charts/salesChart';
+import React from "react";
+import { Card } from "antd";
+import Barchart from "../charts/barchart";
+import Saleschart from "../charts/salesChart";
 const { Meta } = Card;
 const DashboardLowerCards = (data) => {
-  const {data: salesData} = data
+  const { data: salesData } = data;
+  const isMobile = window.innerWidth < 767;
+
   return (
-    <div style={{display : 'flex', gap : 20, height: '100%'}}>
-  <Card
-    title="Platform sales"
-    style={{
-      width: '100%',
-      height: '44vh'
-    }}
-  >
-    <Saleschart salesData={salesData && salesData.salesData}/>
-  </Card>
-  <Card
-    title="Top products"
-    style={{
-        width: '100%',
-        height: '44vh'
-    }}
-  >
-    <Barchart salesData={salesData && salesData.salesData}/>
-  </Card>
-  <Card
-    hoverable
-    style={{
-      width: '30rem',
-      height: '100%'
-    }}
-    cover={<img alt="example" src={salesData?.bestProduct?.image} />}
-  >
-    <Meta title={salesData?.bestProduct?.name} description={`sales: ${salesData?.bestProduct?.reOrdered}`} />
-  </Card>
-  </div>
-)};
+    <div style={{ display: "flex", gap: 20, height: "100%" }}>
+      {isMobile ? (
+        <h2>Please use a larger screen device to view charts.</h2>
+      ) : (
+        <>
+      <Card
+        title="Platform sales"
+        style={{
+          width: "100%",
+          height: "44vh",
+        }}
+      >
+        <Saleschart salesData={salesData && salesData.salesData} />
+      </Card>
+      <Card
+        title="Top products"
+        style={{
+          width: "100%",
+          height: "44vh",
+        }}
+      >
+        <Barchart salesData={salesData && salesData.salesData} />
+      </Card>
+      </>)}
+      <Card
+        hoverable
+        style={{
+          width: "30rem",
+          height: "100%",
+        }}
+        cover={<img alt="example" src={salesData?.bestProduct?.image} />}
+      >
+        <Meta
+          title={salesData?.bestProduct?.name}
+          description={`sales: ${salesData?.bestProduct?.reOrdered}`}
+        />
+      </Card>
+    </div>
+  );
+};
 export default DashboardLowerCards;
--- src/components/charts/salesChart.js
+++ src/components/charts/salesChart.js
@@ -1,12 +1,5 @@
 import React from "react";
-import {
-  AreaChart,
-  Area,
-  CartesianGrid,
-  XAxis,
-  YAxis,
-  Tooltip,
-} from "recharts";
+import { AreaChart, Area, CartesianGrid, XAxis, YAxis, Tooltip } from "recharts";
 
 const Saleschart = ({ salesData }) => {
   return (
@@ -17,17 +10,13 @@ const Saleschart = ({ salesData }) => {
       margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
     >
       <defs>
-        <linearGradient id="colorPrice" x1="0" y1="0" x2="0" y2="1">
+        <linearGradient id="colorPurchased" x1="0" y1="0" x2="0" y2="1">
           <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
           <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
         </linearGradient>
-        <linearGradient id="colorPurchased" x1="0" y1="0" x2="0" y2="1">
+        <linearGradient id="colorReOrdered" x1="0" y1="0" x2="0" y2="1">
           <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
           <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
-        </linearGradient>
-        <linearGradient id="colorReOrdered" x1="0" y1="0" x2="0" y2="1">
-          <stop offset="5%" stopColor="#ffc658" stopOpacity={0.8} />
-          <stop offset="95%" stopColor="#ffc658" stopOpacity={0} />
         </linearGradient>
       </defs>
       <XAxis dataKey="name" />
@@ -36,22 +25,15 @@ const Saleschart = ({ salesData }) => {
       <Tooltip />
       <Area
         type="monotone"
-        dataKey="price"
-        stroke="#8884d8"
-        fillOpacity={1}
-        fill="url(#colorPrice)"
-      />
-      <Area
-        type="monotone"
         dataKey="purchased"
-        stroke="#82ca9d"
+        stroke="#8884d8"
         fillOpacity={1}
         fill="url(#colorPurchased)"
       />
       <Area
         type="monotone"
         dataKey="reOrdered"
-        stroke="#ffc658"
+        stroke="#82ca9d"
         fillOpacity={1}
         fill="url(#colorReOrdered)"
       />
